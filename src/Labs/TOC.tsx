6ee916b9f346eed2f88d1e15a47fb3ecad41bf3c
vaibhav <vaibhavsankaran24@gmail.com>
2025-02-09 00:53:01 -0500

    a2 Final push

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -19,7 +19,7 @@ export default function TOC() {
         <Nav.Link href="#/Kambaz">Kambaz</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="https://github.com/svaibhav07codez">My GitHub</Nav.Link>
+        <Nav.Link id="wd-github" href="https://github.com/svaibhav07codez/kambaz-react-web-app/tree/a2">My GitHub</Nav.Link>
       </Nav.Item>
     </Nav>
  );}
