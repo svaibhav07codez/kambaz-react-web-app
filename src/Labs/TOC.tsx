f5f37facc4a00c1636b94511b73566fcaf4a6244
Vaibhav S <vaibhavsankaran24@gmail.com>
2025-02-19 22:49:39 -0500

    Update TOC.tsx

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -32,7 +32,7 @@ export default function TOC() {
       </Nav.Item>
       <br></br>
       <Nav.Item>
-        <Nav.Link id="wd-github" href="https://github.com/svaibhav07codez/kambaz-react-web-app/tree/a2">My GitHub</Nav.Link>
+        <Nav.Link id="wd-github" href="https://github.com/svaibhav07codez/kambaz-react-web-app/tree/a3">My GitHub</Nav.Link>
       </Nav.Item>
     </Nav>
  );}
