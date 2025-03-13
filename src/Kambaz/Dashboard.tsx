5e1ec04bda0ad52994d4cf0a9c60715d3a0ed7e0
Vaibhav S <vaibhavsankaran24@gmail.com>
2025-03-13 10:46:39 -0400

    Update Dashboard.tsx

--- src/Kambaz/Dashboard.tsx
+++ src/Kambaz/Dashboard.tsx
@@ -46,7 +46,7 @@ export default function Dashboard() {
             <Card>
               <Link to={`/Kambaz/Courses/${course._id}/Home`}
                 className="wd-dashboard-course-link text-decoration-none text-dark">
-                <Card.Img src={course.image} variant="top" width="100%" height={160} />
+                <Card.Img src={course.img} variant="top" width="100%" height={160} />
                 <Card.Body className="card-body">
                   <Card.Title className="wd-dashboard-course-title text-nowrap overflow-hidden">
                     {course.name} 
