68c0f222dd4a2620a4ecf56028a93e8fccfcb3f6
Vaibhav S <vaibhavsankaran24@gmail.com>
2025-03-14 12:50:18 -0400

    new updated code

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -1,9 +1,9 @@
-import { Link, useLocation } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+//import { useSelector } from "react-redux";
 
 export default function AccountNavigation() {
-  const { currentUser } = useSelector((state: any) => state.accountReducer);
-  const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+  //const { currentUser } = useSelector((state: any) => state.accountReducer);
+  //const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
   return (
     <div id="wd-account-navigation">
       <Link to={`/Kambaz/Account/Signin`}> Signin </Link> <br />
